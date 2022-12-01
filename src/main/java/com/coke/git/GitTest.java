220101c64b02f9619de8feab06719b7e37801db1
Coke <1668056731@qq.com>
2022-12-02 03:34:41 +0800

    first

---
+++ src/main/java/com/coke/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.coke.git;
+
+/**
+ * @author Coke
+ * @date 2022/12/2 3:30
+ * FengFeng still love ZaiZai today !
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
