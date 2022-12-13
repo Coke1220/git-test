616e36596bedbf0feaaecb91d542a6a71c2b6a24
Coke <1668056731@qq.com>
2022-12-13 22:44:57 +0800

    hot2

--- src/main/java/com/coke/git/GitTest.java
+++ src/main/java/com/coke/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello git");
         System.out.println("hello hot-fix");
+        System.out.println("hello test");
     }
 }
